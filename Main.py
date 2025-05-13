8db393c4fec77ffe25e4d988a24f5cb012a947e3
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-05-14 00:58:03 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from Handlers import train_status, pnr_status, trains_from_to, fare_enquiry, schedule
 
-app = Client("train_bot", bot_token="YOUR_BOT_TOKEN", api_id=123456, api_hash="YOUR_API_HASH")
+app = Client("train_bot", bot_token="7454714545:AAHIPQHGiujqJmWj-vbd4R3VJIBrjMX8GPM", api_id=6067591, api_hash="94e17044c2393f43fda31d3afe77b26b")
 
 @app.on_message(filters.command("start"))
 async def start(_, message: Message):
