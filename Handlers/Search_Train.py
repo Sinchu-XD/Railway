d870ac87a62f2ded621150d9b550406e3ce1f907
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-05-14 12:33:44 +0530

    Update Search_Train.py

--- Handlers/Search_Train.py
+++ Handlers/Search_Train.py
@@ -17,7 +17,7 @@ def register(app):
             querystring = {"query": query}
 
             headers = {
-                "x-rapidapi-key": "YOUR_RAPIDAPI_KEY",
+                "x-rapidapi-key": "814d366d83msh97b8ba89155c2a8p140352jsn4c9a3b3bb565",
                 "x-rapidapi-host": "irctc1.p.rapidapi.com"
             }
 
